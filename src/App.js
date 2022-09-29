b4159231c34c0488a7538d01b8801f6f644b58e1
SeonWoo Kim <ocean@SeonWooui-MacBookPro.local>
2022-09-29 23:00:10 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import react from 'react';
 import Btn from './component/Btn';
 import styles from './assets/css/App.module.css';
 
 function App() {
+  const [keyword, setKeyword] = react.useState('');
+  const onChange = event => {
+    setKeyword(event.target.value);
+  };
+  console.log('i run all the time');
+  react.useEffect(() => {
+    console.log('CALL THE API');
+  }, []);
+  react.useEffect(() => {
+    console.log('Searching');
+  }, [keyword]);
   return (
     <div>
       <h1 className={styles.title}>Welcome React</h1>
-      <Btn text="hi" />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="search box"
+      />
+      <button>search</button>
+      <div>
+        <Btn text="hi" />
+      </div>
     </div>
   );
 }
